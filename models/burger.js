943a1e7e297ea91e9a86ddd94d23a7e9c3c1b185
Randi Prince <randiprince3@MacBook-Pro-7.local>
2018-09-13 15:13:03 -0700

    route

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.allOfThem("burgers", function(res) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
