185232a8643024d79035a9accb7f1063ff9bef0d
Randi Prince <randiprince3@MacBook-Pro-7.local>
2018-09-13 21:04:29 -0700

    saying hello

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,7 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL) {
+    console.log('\n\n\nHELLLO\n\n\n\n')
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
